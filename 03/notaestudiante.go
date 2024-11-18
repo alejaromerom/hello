87fa27077462baa1a12861da9d760b41bbea6f9f
Alejandra Romero <aleromero43@gmail.com>
2024-11-18 16:45:22 -0500

    simple go connection test

--- 03/notaestudiante.go
+++ 03/notaestudiante.go
@@ -8,7 +8,8 @@ var estudiante  string
 fmt.Print("Escriba el nombre del estudiante: ")
 fmt.Scan(&estudiante)
 
-var nota1, nota2, nota3, notafinal float32
+var nota1, nota2, nota3 float64 
+var notafinal float64
 fmt.Print("Esriba la nota 1: ")
 fmt.Scan(&nota1)
 
@@ -18,14 +19,13 @@ fmt.Scan(&nota2)
 fmt.Print("Esriba la nota 3: ")
 fmt.Scan(&nota3)
 
-notafinal =nota1 + nota2 + nota3
+notafinal =((nota1 + nota2 + nota3)/3)
 
-fmt.Println("su nota final es ", notafinal/3 )
+fmt.Println("su nota final es: ", notafinal)
 
-if (notafinal > 3.0) {
+if (notafinal >= 3.0) {
 fmt.Println("el estudiante", estudiante,"aprueba")
 }else{
 	fmt.Println("el estudiante", estudiante, "no aprueba")
 }
-
 }
